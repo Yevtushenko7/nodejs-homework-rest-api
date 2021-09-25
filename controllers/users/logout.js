99d8778040c08d8dae48fa00505035c9e61897c9
Yevhen Yevtushenko <korish140193@gmail.com>
2021-09-25 14:50:32 +0300

    .

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,7 +1,7 @@
 const { User } = require('../../model');
 const { Unauthorized } = require("http-errors");
 
-const logout = async (req, res, next) => {
+const logout = async (req, res) => {
        const user = await User.findByIdAndUpdate(req.user._id, { token: null });
         if (!user) {
             throw new Unauthorized("Not authorized");
